77f02bb38309c829816b38e01ccc67d0c541f311
Octavio <octaviodesalvatore@gmail.com>
2021-11-01 20:01:11 -0300

    Minor fixes

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,6 +4,46 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#951717",
+        borderRadius: "10px",
+        width: "35px",
+        height: "35px",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#951717",
+        borderRadius: "10px",
+        width: "35px",
+        height: "35px",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
 function Skills() {
   const settings = {
     dots: true,
@@ -11,6 +51,8 @@ function Skills() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
   return (
     <BackgroundImage>
