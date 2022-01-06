e7e91a39cecfd76f309c283b06c6ee1a11b78ebe
Octavio <octaviodesalvatore@gmail.com>
2022-01-06 10:46:44 -0300

    Navbar update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function Navbar() {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset);
+    };
+  }, []);
+
+  console.log(offset);
+
   return (
-    <Container>
+    <Container offset={offset}>
       <Wrapper>
         <Logo>
           <p>octavio.</p>
         </Logo>
         <Nav>
-          <a href="#home-top" className="underlineRed">
+          <UnderlineRedA offset={offset} href="#home-top">
             Home
-          </a>
-          <a href="#about-me-section" className="underlineRed">
+          </UnderlineRedA>
+          <UnderlineRedA offset={offset} href="#about-me-section">
             About me
-          </a>
-          <a href="##my-work" className="underlineRed">
+          </UnderlineRedA>
+          <UnderlineRedA offset={offset} href="##my-work">
             Projects
-          </a>
-          <a href="#contact-me-section" className="contact-me">
+          </UnderlineRedA>
+          <ContactMeA offset={offset} href="#contact-me-section">
             Contact Me
-          </a>
+          </ContactMeA>
         </Nav>
       </Wrapper>
     </Container>
@@ -33,6 +43,16 @@ const Container = styled.div`
   width: 100%;
   position: fixed;
   z-index: 90;
+  transition: all 150ms cubic-bezier(0.77, 0, 0.175, 1);
+
+  ${({ offset }) =>
+    offset > 10 &&
+    `
+    backdrop-filter: saturate(180%) blur(10px);
+    background-color: #020000bd;
+    transition: background-color 250ms ease-in;
+    color: white;
+  `}
 `;
 
 const Wrapper = styled.div`
@@ -60,5 +80,50 @@ const Nav = styled.nav`
   a {
     margin-right: 10px;
     margin-left: 10px;
+  }
+`;
+
+const ContactMeA = styled.a`
+  border: 2px solid #951717;
+  padding: 8px 18px;
+  border-radius: 20px;
+  transition: all 250ms ease-in-out;
+
+  ${({ offset }) =>
+    offset > 10 &&
+    `
+    color: white;
+  `}
+
+  &:hover {
+    background-color: #951717;
+    color: #fff;
+  }
+`;
+
+const UnderlineRedA = styled.a`
+  position: relative;
+  background-color: transparent;
+  border-radius: 20px;
+  transition: all 350ms cubic-bezier(0.77, 0, 0.175, 1);
+  ${({ offset }) =>
+    offset > 10 &&
+    `
+    color: white;
+  `}
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: -10px;
+    left: 0;
+    width: 0px;
+    height: 3px;
+    background-color: #951717;
+    transition: all 0.3s ease-in-out;
+  }
+
+  &:hover::after {
+    width: 100%;
   }
 `;
